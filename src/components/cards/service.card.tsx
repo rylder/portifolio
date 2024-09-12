dadb79ff54cd2aa02d8612c9e6e6e831a867bd59
rylder <rylder2014@gmail.com>
2024-09-11 23:36:14 -0300

    fix: card service height

--- src/components/cards/service.card.tsx
+++ src/components/cards/service.card.tsx
@@ -7,7 +7,7 @@ interface ServiceCardProps {
 
 export default function ServiceCard({ service }: ServiceCardProps) {
   return (
-    <div className="flex h-72 max-w-80 flex-col items-center justify-between rounded bg-gray p-5 shadow-lg shadow-[#1f1f1f]">
+    <div className="flex min-h-full max-w-80 flex-col items-center justify-between rounded bg-gray p-5 shadow-lg shadow-[#1f1f1f]">
       <div className="flex flex-1 flex-col items-center justify-center">
         <div
           className="m-10 size-12 rounded-lg shadow-lg"
